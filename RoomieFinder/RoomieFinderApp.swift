45ec26d983cc3670d9579f034e17cc3785ddea09
Vanessa Tran <vtran1919@gmail.com>
2022-10-22 19:13:06 -0400

    Initial Commit

---
+++ RoomieFinder/RoomieFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomieFinderApp.swift
+//  RoomieFinder
+//
+//  Created by user228000 on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RoomieFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
