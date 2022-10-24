dffaf5f89e5c3ac0f74b00a138cc3c24fcd306eb
Vanessa Tran <vtran1919@gmail.com>
2022-10-24 13:33:20 -0400

    reorganized structure of files, added documentation comments

---
+++ RoomieFinder/LoginView.swift
@@ -0,0 +1,56 @@
+/**
+    Login View Screen for user login into Roomie FInder
+ */
+
+
+import SwiftUI
+
+// Login view screen
+struct LoginView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    @Binding var showFeed: Bool
+    
+    var body: some View {
+        VStack {
+            Text("Roomie Finder")
+                .font(.largeTitle)
+                .bold()
+            TextField("Username", text: $username)
+                .padding()
+                .background()
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            TextField("Password", text: $password)
+                .padding()
+                .background()
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            Button(action: {
+                //TODO: add logic for authentication here or function
+                print("Logged In")
+                self.showFeed = true
+                }) {
+                LoginButton()
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity,maxHeight: .infinity)
+                .accentColor(Color.black)
+                .background(Color.green)
+    }
+}
+
+// Login button styling
+struct LoginButton : View {
+    var body: some View {
+        return Text("LOGIN")
+            .font(.headline)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 200, height: 60)
+            .background(Color.blue)
+            .cornerRadius(10.0)
+    }
+}
