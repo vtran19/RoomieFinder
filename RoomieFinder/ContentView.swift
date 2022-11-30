da469db3569f9df3c4efb21c7fbc8b1c59d0cdb5
stephaniegodes <sgodes@uvm.edu>
2022-11-30 14:02:34 -0500

    added color palette

--- RoomieFinder/ContentView.swift
+++ RoomieFinder/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 import FirebaseDatabase
 
+
+let gray = Color(red: 51/255.0, green: 55/255.0, blue: 69/255.0)
+let pink = Color(red: 230/255.0, green: 55/255.0, blue: 191/255.0)
+let orange = Color(red: 254/255.0, green: 95/255.0, blue: 85/255.0)
+let mint = Color(red: 199/255.0, green: 239/255.0, blue: 207/255.0)
+let eggshell = Color(red: 250/255.0, green: 243/255.0, blue: 221/255.0)
+
 // Overall view
 struct ContentView: View {
     
